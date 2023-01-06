3d6a068597c5aeaab96c3471003f446ae0e94ef5
matheus.silva <tecmatheusg@hotmail.com>
2023-01-06 15:00:17 -0300

    continuação

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,11 +27,12 @@ namespace TelecallUnisuamCRUD.Controllers
         {
             return View();
         }
-
+        /*
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }*/
     }
 }
